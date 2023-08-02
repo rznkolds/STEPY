de5a4fc4689543af93cb6df3985f81c159e5d3fb
Ramazan <rznkold@protonmail.com>
2023-08-02 23:35:43 +0300

    Added Room Dao and Data Store Source files

---
+++ data/src/main/java/com/rznkolds/data/source/room/StepDao.kt
@@ -0,0 +1,28 @@
+package com.rznkolds.data.source.room
+
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import com.rznkolds.data.dto.Step
+@Dao
+interface StepDao {
+
+    // Get and add step method for dao of the room
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun addStep(step: Step)
+
+    @Query("SELECT * FROM steps WHERE day=:day AND month=:month AND year=:year")
+    fun getStep(day: Int, month: Int, year: Int): Step
+
+    // Get week steps method for dao of the room
+
+    @Query("SELECT * FROM ( SELECT * FROM steps ORDER BY id DESC LIMIT 7 ) sub ORDER BY id ASC")
+    fun getWeekSteps(): List<Step>
+
+    // Get month steps method for dao of the room
+
+    @Query("SELECT * FROM ( SELECT * FROM steps ORDER BY id DESC LIMIT 30 ) sub ORDER BY id ASC")
+    fun getMonthSteps(): List<Step>
+}
